f85cda2b86f513813605390a16d29fb36b7abd3b
kenmartey <kenmartey89@gmail.com>
2022-07-17 22:36:05 -0400

    Fixed metamask connection issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import CryptoKoffeeContract from "./contracts/CryptoKoffee.json";
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = { walletInfo: {name: "", link: "", walletAddress: "", walletBalance: "", numOfDonations: ""}, web3: null, accounts: null, contract: null };
 
   componentDidMount = async () => {
     try {
@@ -17,9 +17,9 @@ class App extends Component {
 
       // Get the contract instance.
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
+      const deployedNetwork = CryptoKoffeeContract.networks[networkId];
       const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
+        CryptoKoffeeContract.abi,
         deployedNetwork && deployedNetwork.address,
       );
 
@@ -35,18 +35,21 @@ class App extends Component {
     }
   };
 
-  // runExample = async () => {
-  //   const { accounts, contract } = this.state;
+  runExample = async () => {
+    const { accounts, contract } = this.state;
 
-  //   // Stores a given value, 5 by default.
-  //   await contract.methods.set(5).send({ from: accounts[0] });
+    // Stores a given value, 5 by default.
+    await contract.methods.createWallet("walletName", "Link").send({ from: accounts[0] });
 
-  //   // Get the value from the contract to prove it worked.
-  //   const response = await contract.methods.get().call();
+    // Get the value from the contract to prove it worked.
+    const response = await contract.methods.getWallet().call();
 
-  //   // Update state with the result.
-  //   this.setState({ storageValue: response });
-  // };
+    // Update state with the result.
+    console.log(response)
+    this.setState({ walletInfo: {name: response.name, link: response.link, 
+      walletAddress: response.walletAddress, walletBalance: response.walletBalance.valueOf(), 
+      numOfDonations: response.numOfDonations} });
+  };
 
   render() {
     if (!this.state.web3) {
@@ -54,17 +57,17 @@ class App extends Component {
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
+        <h1>Yeeaayy CryptoKoffee is Here!</h1>
+        <p>Your wallet has been created and ready to accept donations.</p>
+       
         <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
+        <h2>Wallet created details</h2>
         </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <div>Wallet Identifier: {this.state.walletInfo.name}</div>
+        <div>Wallet Link hashed: {this.state.walletInfo.link}</div>
+        <div>Wallet Address: {this.state.walletInfo.walletAddress}</div>
+        <div>Donation Balance: {this.state.walletInfo.walletBalance}</div>
+        <div>Number of Donations: {this.state.walletInfo.numOfDonations}</div>
       </div>
     );
   }
